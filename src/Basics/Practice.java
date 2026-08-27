53a383aef15d608eb4244ed6ba03b78de74154d1
code with kemi <kemikumari59@gmail.com>
2026-08-27 21:27:02 +0530

    Initial commit

---
+++ src/Basics/Practice.java
@@ -0,0 +1,8 @@
+void main() {
+    System.out.println("kemi kumari");
+    System.out.print(5);
+    System.out.println("5+5");
+
+    System.out.println(4+5);// here 4+3 is not a string .this is a expression which is firstly solve and then execute.
+
+}
---
+++ src/Variable.java
@@ -0,0 +1,29 @@
+public class Variable {
+    static void main() {
+        //create // declare
+        int age;
+        //System.out.println(age);
+        //assign value
+        age = 11;
+        System.out.println(age);
+        //define or initialize
+        int totalMarks = 20;
+        System.out.println(totalMarks);
+        //Case Sensitive
+        int weight = 78;
+        int WEIGHT = 56;
+        System.out.println("weight:" + weight);
+        System.out.println("weight:" + WEIGHT);
+        //Subsequent Letters
+        int height5 = 21;
+        //No Reserved keywords
+        // int class = 45;
+        //length
+        int hello = 100;
+        int a = 1;
+        int b = 2;
+
+
+
+    }
+}
